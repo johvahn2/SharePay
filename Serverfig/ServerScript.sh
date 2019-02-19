e88ec70c735db99640ac49ae9dd47795f4f1a24a
Johvahn Brodie <johvahn14@gmail.com>
2019-02-18 23:58:01 -0400

    aa

--- Serverfig/ServerScript.sh
+++ Serverfig/ServerScript.sh
@@ -6,4 +6,5 @@ mv /prod/stage/* /prod/
 cd /prod
 rm -rf stage
 npm install
+NODE_ENV=prod STRIPE_PUBLISHABLE_KEY=pk_live_EHXVwoPCMfJQEbxZhf6va73M STRIPE_SECRET_KEY=sk_live_cLXqHP6gTcFlPljG8Z4mAXjv pm2 start server.js
 echo "DONE!"
